4cbbb6ac6b178ceaf8155231a8fd5697a6158b45
Celina Garcia Paramo <mcelina_21@hotmail.com>
2015-10-29 15:03:08 -0600

    Nuevas escenas

---
+++ Personaje/Assets/Scripts/Botones.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Botones: MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//SmoveForward ();
+	}
+	//Funcion mueve al personaje
+	public void moveL()
+	{
+		Debug.Log ("Holiwi");
+		this.transform.Translate((Time.deltaTime * speed)* Vector3.left);
+	}
+	//Funcion mueve al personaje
+	public void moveR()
+	{
+		Debug.Log ("Holiwi");
+		this.transform.Translate((Time.deltaTime * speed)* Vector3.right);
+	}
+}
---
+++ Personaje/Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveForward ();
+	}
+	//Funcion mueve al personaje
+	public void moveForward()
+	{
+		Debug.Log ("Holiwi");
+		this.transform.Translate((Time.deltaTime * speed)* Vector3.down);
+	}
+}
